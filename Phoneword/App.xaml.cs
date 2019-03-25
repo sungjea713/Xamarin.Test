25daefde9ec11c1262510161b230f242a8a47a6d
sungjea713 <sungjea713@gmail.com>
2019-03-25 11:10:48 +0900

    Initial check-in of module Phoneword

---
+++ Phoneword/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Phoneword
+{
+    public partial class App : Application
+    {
+        public static IList<string> PhoneNumbers { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+            PhoneNumbers = new List<string>();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Phoneword/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public partial class MainPage : ContentPage
+    {
+        string translateNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnTranslate(object sender, EventArgs e)
+        {
+
+        }
+
+        void OnCall(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
